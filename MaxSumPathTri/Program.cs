cf6ff22ed4187f2d9ef091b13ea1430f6e18ef22
Gudbrandr42 <mark.bullard42@gmail.com>
2020-03-30 10:51:22 -0400

    setup and solution added for project

---
+++ MaxSumPathTri/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaxSumPathTri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] list = new int[3, 4];
+            string input = @"1
+               1 2
+               1 3 4";
+            var charArray = input.Split('\n');
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                var numArr = charArray[i].Trim().Split(' ');
+
+                for (int j = 0; j < numArr.Length; j++)
+                {
+                    int number = Convert.ToInt32(numArr[j]);
+                    list[i, j] = number;
+                }
+            }
+
+            for (int i = 1; i >= 0; i--)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    list[i, j] = Math.Max(list[i, j] + list[i + 1, j], list[i, j] + list[i + 1, j + 1]);
+                }
+            }
+            Console.WriteLine(string.Format("Maximum total: {0}", list[0, 0]));
+        }
+    }
+}
